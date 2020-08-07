c07a56114ad2fa09e867d5877efbba62c5c86253
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-08-07 00:33:30 -0300

    Aula 02: Olhando as oportunidades

---
+++ server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,9 @@
+function convertHoursToMinutes(time: string) {
+  const [hours, minutes] = time.split(':').map(Number);
+
+  const timeInMinutes = hours * 60 + minutes;
+
+  return timeInMinutes;
+}
+
+export default convertHoursToMinutes;
